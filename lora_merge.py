bac9a3b1aec7a0b3501d7b6bd9342180061e27fb
laksjdjf <aikasama@hotmail.co.jp>
2023-10-17 00:41:56 +0900

    add lora resize

--- lora_merge.py
+++ lora_merge.py
@@ -13,7 +13,6 @@ class LoraMerger:
         return {
             "required": {
                 "lora_1": ("LoRA",),
-                "lora_2": ("LoRA", ),
                 "mode": (["add", "concat", "svd"], ),
                 "rank": ("INT", {
                     "default": 16, 
@@ -22,27 +21,39 @@ class LoraMerger:
                     "step": 1, #Slider's step
                     "display": "number" # Cosmetic only: display as "number" or "slider"
                 }),
+                "threshold": ("FLOAT", {
+                    "default": 1.0,
+                    "min": 0,
+                    "max": 1,
+                    "step": 0.01,
+                }),
                 "device": (["cuda", "cpu"], ),
                 "dtype": (["float32", "float16", "bfloat16"], ),
             },
+            "optional": {
+                "lora_2": ("LoRA",),
+            }
         }
     RETURN_TYPES = ("LoRA", )
     FUNCTION = "lora_merge"
 
     CATEGORY = "lora_merge"
 
-    def lora_merge(self, lora_1, lora_2, mode, rank, device, dtype):
+    def lora_merge(self, lora_1, lora_2=None, mode=None, rank=None, threshold=None, device=None, dtype=None):
         
-        lora = self.merge(lora_1, lora_2, mode, rank, device, dtype)
+        lora = self.merge(lora_1, lora_2, mode, rank, threshold, device, dtype)
 
         return (lora, )
     
     @torch.no_grad()
-    def merge(self, lora_1, lora_2, mode, rank, device, dtype):
+    def merge(self, lora_1, lora_2, mode, rank, threshold, device, dtype):
         # lora = up @ down * alpha / rank
 
         weight = {}
         dtype = torch.float32 if dtype == "float32" else torch.float16 if dtype == "float16" else torch.bfloat16
+
+        if lora_2 is None:
+            lora_2 = {"lora":{}, "strength_model":0, "strength_clip":0}
 
         keys_1 = [key[: key.rfind(".lora_down")] for key in lora_1["lora"].keys() if ".lora_down" in key]
         keys_2 = [key[: key.rfind(".lora_down")] for key in lora_2["lora"].keys() if ".lora_down" in key]
@@ -55,8 +66,12 @@ class LoraMerger:
         for key in keys:
             if key not in keys_1:
                 up, down, alpha = calc_up_down_alpha(key, lora_2)
+                if mode == "svd":
+                    up, down = svd_merge(up, down, None, None, rank, threshold, device)
             elif key not in keys_2:
                 up, down, alpha = calc_up_down_alpha(key, lora_1)
+                if mode == "svd":
+                    up, down = svd_merge(up, down, None, None, rank, threshold, device)
             else:
                 up_1, down_1, alpha_1 = calc_up_down_alpha(key, lora_1, add=mode!="add")
                 up_2, down_2, alpha_2 = calc_up_down_alpha(key, lora_2, add=mode!="add")
@@ -88,7 +103,7 @@ class LoraMerger:
                     up = torch.cat([up_1*scale_1, up_2*scale_2], dim=1)
                     down = torch.cat([down_1*scale_1, down_2*scale_2], dim=0)
                 elif mode == "svd":
-                    up, down = svd_merge(up_1, down_1, up_2, down_2, rank, device)
+                    up, down = svd_merge(up_1, down_1, up_2, down_2, rank, threshold, device)
                 
             weight[key + ".lora_up.weight"] = up
             weight[key + ".lora_down.weight"] = down
@@ -116,26 +131,41 @@ def calc_up_down_alpha(key, lora, add=True):
 
     return up, down, alpha
 
+# frovenius normによるrankの計算
+def index_sv_fro(S, target):
+    S_squared = S.pow(2)
+    s_fro_sq = float(torch.sum(S_squared))
+    sum_S_squared = torch.cumsum(S_squared, dim=0)/s_fro_sq
+    index = int(torch.searchsorted(sum_S_squared, target**2)) + 1
+    index = max(1, min(index, len(S)-1))
+
+    return index
+
 @torch.no_grad()
-def svd_merge(up_1, down_1, up_2, down_2, rank, device):
+def svd_merge(up_1, down_1, up_2, down_2, rank, threshold, device=None):
     org_device = up_1.device
     org_dtype = up_1.dtype
 
     up_1 = up_1.to(device)
     down_1 = down_1.to(device)
-    up_2 = up_2.to(device)
-    down_2 = down_2.to(device)
-
     r_1 = up_1.shape[1]
-    r_2 = up_2.shape[1]
-
     weight_1 = up_1.view(-1, r_1) @ down_1.view(r_1, -1)
-    weight_2 = up_2.view(-1, r_2) @ down_2.view(r_2, -1)
-    weight = weight_1 * rank / r_1 + weight_2 * rank / r_2
+
+    if up_2 is not None:
+        up_2 = up_2.to(device)
+        down_2 = down_2.to(device)
+        r_2 = up_2.shape[1]
+        weight_2 = up_2.view(-1, r_2) @ down_2.view(r_2, -1)
+        weight = weight_1 / r_1 + weight_2 / r_2
+    else:
+        weight = weight_1 / r_1
 
     weight = weight.to(dtype=torch.float32) # SVD only supports float32
 
     U, S, Vh = torch.linalg.svd(weight)
+
+    if threshold < 1:
+        rank = index_sv_fro(S, threshold) + 1
 
     U = U[:, :rank]
     S = S[:rank]
@@ -154,7 +184,7 @@ def svd_merge(up_1, down_1, up_2, down_2, rank, device):
         U = U.reshape(up_1.shape[0], rank, 1, 1)
         Vh = Vh.reshape(rank, down_1.shape[1], down_1.shape[2], down_1.shape[3])
 
-    up = U.to(org_device, dtype=org_dtype)
-    down = Vh.to(org_device, dtype=org_dtype)
+    up = U.to(org_device, dtype=org_dtype) * math.sqrt(rank)
+    down = Vh.to(org_device, dtype=org_dtype) * math.sqrt(rank)
 
     return up, down
